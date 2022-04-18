db0de75c13397bda65c9ffdd0b8ae027f7979a1b
Trajce <tartulian69@gmail.com>
2022-04-18 13:45:30 +0000

    changes made

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var res2="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML=res2;
+
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
