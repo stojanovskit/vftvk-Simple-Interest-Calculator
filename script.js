f1f86e0ace78668910dcdd621f4b155ad70c4d83
John Doe <johndoe@example.com>
2022-04-19 06:57:55 +0200

    try 3

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    var principal = document.getElementById("principal").value;
+    var principal = document.getElementById("principal");
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
+    var interest = principal.value * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    var neg = document.getElementById("principal");
-    var amount = principal * (rate/100) * years;
-    if (principal < 1) {
+    
+    var amount = principal.value * (rate/100) * years;
+    if (principal.value < 1) {
         alert("Enter a positive number.");
-        neg.focus();
+        principal.focus();
+        return false;
+        
         
     }
     else{
-    document.getElementById("result").innerHTML= "<br> <br> If you deposit " + principal + ", <br> at an interest rate of " + rate + " <br> You will receive an amount of " + amount  +",<br>in the year <span class=\"resul\t">"+year+" </span>\<br\>" ;
+    document.getElementById("result").innerHTML= "<span class=\"wh\"> <br> If you deposit </span>" + principal.value + "<span class=\"wh\">, <br> at an interest rate of </span>" + rate + "<span class=\"wh\"> <br> You will receive an amount of </span>" + amount  +"<span class=\"wh\">,<br>in the year </span>" + year + "\<br\>" ;
     }
 }
 function updateRate() 
@@ -22,4 +24,3 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-        
